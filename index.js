4710844ade623fc63c40681644a5d8cfd4ffefab
ezekielswanson <swansonezekiel@gmail.com>
2023-08-23 20:41:25 -0500

    updated js

--- index.js
+++ index.js
@@ -49,21 +49,34 @@ renderMenuItems();
 //need to put data attributes on btns to access the specific btns
 document.addEventListener('click', function(e){
     //we need to pass t/ btns data att. somewhere
-   getMatchingItem(e.target.dataset.foodItem)
-   console.log(e.target.dataset.foodItem)
+  const matchedItem = getMatchingItem(e.target.dataset.foodItem);
+  console.log(matchedItem)
 })
 
 
 
 //set filter based off of the id
-function getMatchingItem(item) {
-    //
+function getMatchingItem(foodId) {
+
+    const foodIdNumber = parseInt(foodId, 10)
+    //filter function
+    const trgtFoodItem = menuArray.filter(function(item){
+        return item.id === foodIdNumber
+    })[0]
+
+    return trgtFoodItem;
+
 }
 
 
 
-//maybe sep func when user btn display complete order 
 
+/*
+//have matching food if pass into another function that display the order totla
+function showOrderTotal() {
+    //
+}
+*/
 
 
 
